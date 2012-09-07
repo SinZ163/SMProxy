a60d5b6b8d9d3e14165fde5d0b30d0f9b6113686
Drew DeVault <sir@cmpwn.com>
2012-09-07 17:03:37 -0600

    Adding more packets

---
+++ SMProxy/Packets/SpawnNamedEntityPacket.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace SMProxy.Packets
+{
+    public class SpawnNamedEntityPacket : Packet
+    {
+        public int EntityId;
+        public string PlayerName;
+        public Vector3 Position;
+        public float Yaw;
+        public float Pitch;
+        public short CurrentItem;
+        // Metadata
+
+        public override byte PacketId
+        {
+            get { return 0x14; }
+        }
+
+        public override int TryReadPacket(byte[] buffer, int length)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
